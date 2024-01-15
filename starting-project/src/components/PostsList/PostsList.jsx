f8404662230dbabd5341985794b3f99e97e440e2
i.kłusek <J6ZkqwVUn>
2024-01-15 21:46:53 +0100

    RLP-02-Fetching and useEffect

--- starting-project/src/components/PostsList/PostsList.jsx
+++ starting-project/src/components/PostsList/PostsList.jsx
@@ -6,13 +6,29 @@ import { useEffect, useState } from 'react';
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    setIsFetching(true);
+    fetchPosts();
+  }, []);
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
     setPosts((prev) => [postData, ...prev]);
   }
-  useEffect(() => {
-    console.log(posts);
-  }, [posts]);
 
   return (
     <>
@@ -21,16 +37,21 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map(({ author, body }, index) => (
             <Post key={index} author={author} body={body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>No Posts yet</h2>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h1>Loading posts...</h1>
         </div>
       )}
     </>
