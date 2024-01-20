748dfcbddc8fd147bac957042c8a12287b7ae497
i.kłusek <J6ZkqwVUn>
2024-01-20 20:30:01 +0100

    NLP-01-Revalidation cache for production and metadata

--- 02-onwards-foodies/app/layout.js
+++ 02-onwards-foodies/app/layout.js
@@ -1,6 +1,22 @@
 import MainHeader from '../components/main-header/main-header';
 import './globals.css';
 
+// https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+// export const metadata = {
+//   generator: 'Next.js',
+//   applicationName: 'Next.js',
+//   referrer: 'origin-when-cross-origin',
+//   keywords: ['Next.js', 'React', 'JavaScript'],
+//   authors: [{ name: 'Seb' }, { name: 'Josh', url: 'https://nextjs.org' }],
+//   creator: 'Jiachi Liu',
+//   publisher: 'Sebastian Markbåge',
+//   formatDetection: {
+//     email: false,
+//     address: false,
+//     telephone: false,
+//   },
+// }
+
 export const metadata = {
   title: 'NextLevel Food',
   description: 'Delicious meals, shared by a food-loving community.',
