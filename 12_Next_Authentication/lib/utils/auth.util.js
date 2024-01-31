6dd4f802a6a5f9a522b6387bc67085899bd678f5
i.kłusek <J6ZkqwVUn>
2024-01-31 10:02:48 +0100

    NLP-1503-Signin with NextAuth authentication process

--- 12_Next_Authentication/lib/utils/auth.util.js
+++ 12_Next_Authentication/lib/utils/auth.util.js
@@ -1,10 +1,16 @@
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
 
 async function hashPassword(password) {
   const hashedPassword = hash(password, 12);
   return hashedPassword;
 }
 
+async function verifyPassword(password, hashedPassword) {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+}
+
 export default {
   hashPassword,
+  verifyPassword,
 };
---
+++ 12_Next_Authentication/pages/api/auth/[...nextauth].js
@@ -0,0 +1,45 @@
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import dbService from '../../../lib/service/db.service';
+import authUtil from '../../../lib/utils/auth.util';
+
+function throwError(client, message) {
+  console.log(message);
+  client.close();
+  //   When we throw error nextAuth by default want's to redirect to errorPage, we can stop that on client side
+  // auth-form signIn('credentials', {redirect: false})
+  throw new Error(message);
+}
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    jwt: true,
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const client = await dbService.connectDatabase();
+
+        const user = await client
+          .db()
+          .collection('users')
+          .findOne({ email: credentials.email });
+        console.log(user);
+        if (!user) throwError(client, 'No user found!');
+
+        const isValid = await authUtil.verifyPassword(
+          credentials.password,
+          user.password
+        );
+        if (!isValid) throwError(client, 'Wrong password');
+
+        client.close();
+        //If we return object from authorize method, we let NextAuth know that authorization succeded
+        return { email: user.email };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
